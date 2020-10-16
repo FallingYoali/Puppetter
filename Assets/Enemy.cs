a45d6b03617379c2d622e8c6fa354e66ccf91422
Ivanmortr <57369225+Ivanmortr@users.noreply.github.com>
2020-10-16 14:57:05 -0500

    duendecinscript

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,38 +1,138 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class Enemy : MonoBehaviour
 {
 
+    public NavMeshAgent agent;
 
-    private Transform player;
-    private float dist;
-    public float moveSpeed;
-    public float howClose;
+    public Transform player;
 
-    // Start is called before the first frame update
-    void Start()
+    public LayerMask esTierra, esJugador;
+
+    public float hp;
+
+
+    //Patrullar 
+    public Vector3 walkpoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+
+    //atacar
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    public GameObject projectile;
+
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerEnRango, playerEnRangoAtaque;
+
+
+    private void Awake()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
+        player = GameObject.Find("Bobby").transform;
+        agent = GetComponent<NavMeshAgent>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        dist = Vector3.Distance(player.position, transform.position);
+        playerEnRango = Physics.CheckSphere(transform.position, sightRange, esJugador);
+        playerEnRangoAtaque = Physics.CheckSphere(transform.position, attackRange, esJugador);
 
-        if(dist <= howClose)
+        if (!playerEnRango && !playerEnRangoAtaque) Patrullar();
+        if (playerEnRango && !playerEnRangoAtaque) PerseguirJugador();
+        if (playerEnRango && playerEnRangoAtaque) AtacarJugador();
+    }
+
+
+    private void Patrullar()
+    {
+        if (!walkPointSet) BuscarWalkPoint();
+
+
+        if (walkPointSet)
+            agent.SetDestination(walkpoint);
+
+        Vector3 distanciaParaPunto = transform.position - walkpoint;
+
+        if (distanciaParaPunto.magnitude < 1f)
+            walkPointSet = false;
+    }
+
+    private void BuscarWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+
+        walkpoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+
+        if (Physics.Raycast(walkpoint, -transform.up, 2f, esTierra))
+
+            walkPointSet = true;
+
+
+    }
+
+
+    private void PerseguirJugador()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AtacarJugador()
+    {
+
+
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if(!alreadyAttacked)
         {
-            transform.LookAt(player);
-            GetComponent<Rigidbody>().AddForce(transform.forward * moveSpeed);
+
+
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
         }
+    }
 
-        if(dist <= 1.5)
-        {
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
 
-            //do damage or explode
+    public void TakeDamage(int damage)
+    {
+        hp -= damage;
 
-        }
+        if (hp <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
+    }
+    
+
+    private void DestroyEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+
     }
 }
