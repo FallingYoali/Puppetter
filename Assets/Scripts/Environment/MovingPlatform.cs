71e2bda9347e7682ff9faa01cd7e8e074a419b9f
FallingYoali <57368325+FallingYoali@users.noreply.github.com>
2020-10-19 17:56:04 -0500

    Moving Platform

---
+++ Assets/Scripts/Environment/MovingPlatform.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    [SerializeField]
+    private float low_y, high_y, speed;  //how high and low the platform will reach
+    private float rateVelocity, time;
+    private Vector3 startPos, endPos;
+    private bool on, goingDown;
+
+    private void Start()
+    {
+        
+    }
+    public void MovePlatform()
+    {
+        Debug.Log("platform moving");
+        startPos = transform.position;
+        endPos = new Vector3(transform.position.x, low_y, transform.position.z);
+        
+        rateVelocity = 1f / Vector3.Distance(startPos, endPos) * speed;
+        
+        time = 0.0f;
+        goingDown = true;
+        on = true;
+    }
+
+    public void StopPlatform()
+    {
+        on = false;
+        time = 0.0f;
+    }
+
+    private void Update()
+    {
+        if (on)
+        {
+            if (time >= 0.5f)
+            {
+                goingDown = !goingDown;
+                time = 0.0f;
+            }
+            if (time <= 1.0f)
+            {
+                Debug.Log("in time");
+                if (goingDown)
+                {
+                    Debug.Log("down");
+                    startPos = transform.position;
+                    endPos = new Vector3(transform.position.x, low_y, transform.position.z);
+                }
+                else
+                {
+                    Debug.Log("up");
+                    startPos = transform.position;
+                    endPos = new Vector3(transform.position.x, high_y, transform.position.z);
+                }
+                time += Time.deltaTime * rateVelocity;
+                transform.position = Vector3.Lerp(startPos, endPos, time);
+            }        
+        }
+    }
+}
---
+++ Assets/Scripts/Environment/Switch.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform platform;
+    [SerializeField]
+    Collider wall, floor;
+    Animation anim;
+    bool on = false, action = false;
+    MovingPlatform movePlatform;
+
+
+    void Start()
+    {
+        Physics.IgnoreCollision(wall, GetComponent<Collider>());
+        Physics.IgnoreCollision(floor, GetComponent<Collider>());
+        anim = gameObject.transform.GetComponentInParent<Animation>();
+        movePlatform = platform.gameObject.GetComponent<MovingPlatform>();
+    }
+
+    void MoveSwitch()
+    {
+        if (!on)
+        {
+            anim["SwitchDown"].time = 0;
+            anim["SwitchDown"].speed = 1;
+            on = true;
+            movePlatform.MovePlatform();
+        }
+        else
+        {
+            anim["SwitchDown"].time = anim["SwitchDown"].length;
+            anim["SwitchDown"].speed = -1;
+            on = false;
+            movePlatform.StopPlatform();
+
+        }
+        anim.Play();
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            action = true;
+        }  
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            action = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (action &&  Input.GetKeyDown(KeyCode.Space))  
+        {
+            Debug.Log("switch");
+            MoveSwitch();
+        }
+    }
+}
