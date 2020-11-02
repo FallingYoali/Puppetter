6bc5a22ce3dba184b6fee59e55477f95589f5f4c
FallingYoali <57368325+FallingYoali@users.noreply.github.com>
2020-11-02 11:18:29 -0600

    keys

---
+++ Assets/Scripts/Environment/key.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class key : MonoBehaviour
+{
+    [SerializeField]
+    private KeyManager keys;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            keys.GrabKey();
+            Destroy(this.gameObject);
+        }
+    }
+
+
+}
